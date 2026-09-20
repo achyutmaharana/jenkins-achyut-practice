d2d5b8c0dc5bbdbc42ceabab6bc4024e325107b5
achyutmaharana <achyutmaharana75@gmail.com>
2026-09-20 13:23:19 +0530

    Create test_app.py

---
+++ docker-pipeline/test_app.py
@@ -0,0 +1,21 @@
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.data == b"Hello from Jenkins + Docker!"
+
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.data == b"Application is healthy"
+
+
+if __name__ == "__main__":
+    test_home()
+    test_health()
+    print("All tests passed!")
